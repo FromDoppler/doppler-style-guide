858c7e23ff9e9d1e890209b353bc968d58913c49
Cromeror <cristobalromerorossi@gmail.com>
2022-05-05 16:59:43 -0500

    feat: initialize the style guide script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./index.scss";
+
+if(process.env.NODE_ENV === "development"){
+  if(!window.dopplerStyleGuideInit){
+    require("./js");
+  }
+  window.dopplerStyleGuideInit = true;
+}else if(window["style-guide-configuration"]?.autoInitialize){
+  require("./js");
+}
