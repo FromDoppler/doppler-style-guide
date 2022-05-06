62a1a287bdfc29acceb3559bb1af2fa7425912d1
Andrés Moschini <andresmoschini@gmail.com>
2022-05-06 09:07:52 -0300

    chore: prettier fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import "./index.scss";
 
-if(process.env.NODE_ENV === "development"){
-  if(!window.dopplerStyleGuideInit){
+if (process.env.NODE_ENV === "development") {
+  if (!window.dopplerStyleGuideInit) {
     require("./js");
   }
   window.dopplerStyleGuideInit = true;
-}else if(window["style-guide-configuration"]?.autoInitialize){
+} else if (window["style-guide-configuration"]?.autoInitialize) {
   require("./js");
 }
