1685af0e2ea828aa6aa2501625a4f2466e46d839
Andrés Moschini <andresmoschini@gmail.com>
2022-05-06 10:20:39 -0300

    WIP

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import "./index.scss";
+import { initializeLegacyJs } from "./js";
 
-if (process.env.NODE_ENV === "development") {
-  if (!window.dopplerStyleGuideInit) {
-    require("./js");
-  }
-  window.dopplerStyleGuideInit = true;
-} else if (window["style-guide-configuration"]?.autoInitialize) {
-  require("./js");
+
+console.log("* 1 *");
+
+// if (process.env.NODE_ENV === "development") {
+//   console.log("* 2 *");
+//   if (!window.dopplerStyleGuideInit) {
+//     console.log("* 3 *");
+//     initializeLegacyJs();
+//   }
+//   window.dopplerStyleGuideInit = true;
+// } else if (window["style-guide-configuration"]?.autoInitialize) {
+//   console.log("* 3 *");
+//   initializeLegacyJs();
+// }
+
+window.initializeLegacyJs = initializeLegacyJs;
+
+if (window["style-guide-configuration"]?.autoInitialize) {
+  console.log("* 4 *");
+  initializeLegacyJs();
 }
+
+console.log("* 5 *");
