99cd444d38f590b84f5975a2e580c22350c4875b
fgonzalez <fgonzalez@makingsense.com>
2025-10-08 10:01:51 -0300

    feat: add tootlip to storybook
    feat: add tootlip to storybook

---
+++ src/stories/Tooltips.stories.js
@@ -0,0 +1,15 @@
+import { Tooltips } from "./Tooltips.js";
+
+// More on default export:
+// https://storybook.js.org/docs/web-components/writing-stories/introduction#default-export
+export default {
+  title: "Components/Tooltips",
+  // More on argTypes: https://storybook.js.org/docs/web-components/api/argtypes
+  argTypes: {},
+};
+
+// More on component templates:
+// https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
+const Template = (args) => Tooltips(args);
+
+export const Default = Template.bind({});
