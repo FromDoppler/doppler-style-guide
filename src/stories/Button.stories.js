e4f544492300f95f5316fef88be5aafcedec320c
emoralesms <emorales@makingsense.com>
2025-10-14 16:44:28 -0300

    fix: argtypes only for icon

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,17 +1,10 @@
 import { Button } from "./Button";
 
-// More on default export:
-// https://storybook.js.org/docs/web-components/writing-stories/introduction#default-export
+// Default export
 export default {
   title: "Components/Button",
-  // More on argTypes: https://storybook.js.org/docs/web-components/api/argtypes
   argTypes: {
     onClick: { action: "onClick" },
-    size: {
-      control: { type: "select" },
-      options: ["small", "medium", "big"],
-      if: { arg: "onlyIcon", truthy: false },
-    },
     color: {
       control: { type: "select" },
       defaultValue: "green",
@@ -22,33 +15,19 @@ export default {
       defaultValue: "primary",
       options: ["primary", "secondary", "link"],
     },
+    size: {
+      control: { type: "select", options: ["small", "medium", "big"] },
+      table: { disable: false },
+    },
     label: {
       control: "text",
-      if: { arg: "onlyIcon", truthy: false },
       description: "Button text",
-    },
-    onlyIcon: {
-      control: "boolean",
-      name: "Only Icon",
-      description: "Show only icon instead of label",
-    },
-    onlyIconSize: {
-      control: {
-        type: "select",
-        options: [
-          0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60, 66, 72, 78, 84, 90, 96, 102,
-          108, 114, 120, 126, 132, 138, 144, 150, 156, 162, 168, 174,
-        ],
-      },
-      name: "Only Icon Size",
-      description: "Padding value (allowed multiples of 6 from $spaceamounts)",
-      if: { arg: "onlyIcon", truthy: true },
+      table: { disable: false },
     },
   },
 };
 
-// More on component templates:
-// https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
+// Component template
 const Template = (args) => {
   let newArgs = { ...args };
   if (args.mode === "link") {
@@ -58,31 +37,83 @@ const Template = (args) => {
   return Button(newArgs);
 };
 
+// Common args
 const commonArgs = {
   disabled: false,
   rounded: false,
   label: "Doppler button",
-  onlyIcon: false,
-  onlyIconSize: 8,
 };
 
+// Primary story
 export const Primary = Template.bind({});
 Primary.args = {
   ...commonArgs,
   loading: false,
   preloading: false,
 };
+Primary.parameters = {
+  ...Primary.parameters,
+  controls: {
+    ...Primary.parameters?.controls,
+    exclude: ["onlyIcon", "onlyIconSize"],
+  },
+};
 
+// Secondary story
 export const Secondary = Template.bind({});
-Secondary.args = { ...commonArgs, mode: "secondary" };
+Secondary.args = {
+  ...commonArgs,
+  mode: "secondary",
+  loading: false,
+  preloading: false,
+};
+Secondary.parameters = {
+  ...Secondary.parameters,
+  controls: {
+    ...Secondary.parameters?.controls,
+    exclude: ["onlyIcon", "onlyIconSize"],
+  },
+};
 
+// Link story
 export const Link = Template.bind({});
-Link.args = { ...commonArgs, mode: "link" };
+Link.args = { ...commonArgs, mode: "link", loading: false, preloading: false };
+Link.parameters = {
+  ...Link.parameters,
+  controls: {
+    ...Link.parameters?.controls,
+    exclude: ["onlyIcon", "onlyIconSize"],
+  },
+};
 
+// Icon story
 export const Icon = Template.bind({});
 Icon.args = {
   ...commonArgs,
   iconClass: "iconapp-user-interface",
-  onlyIcon: false,
-  onlyIconSize: 10,
+  onlyIcon: true,
+  onlyIconSize: 12,
+};
+
+// ArgTypes only for Icon
+Icon.argTypes = {
+  onlyIcon: {
+    control: "boolean",
+    name: "Only Icon",
+    description: "Show only icon instead of label",
+  },
+  onlyIconSize: {
+    control: {
+      type: "select",
+      options: [
+        0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60, 66, 72, 78, 84, 90, 96, 102,
+        108, 114, 120, 126, 132, 138, 144, 150, 156, 162, 168, 174,
+      ],
+    },
+    name: "Only Icon Size",
+    description: "Padding value (allowed multiples of 6)",
+    if: { arg: "onlyIcon", truthy: true },
+  },
+  label: { table: { disable: true } },
+  size: { table: { disable: true } },
 };
